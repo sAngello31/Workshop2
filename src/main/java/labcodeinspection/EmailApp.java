c19ad9fd0534167b17d2c6a62bfd8cd700280296
Angello de Jesus Bravo Mera <bravoangello31@gmail.com>
2024-06-06 20:18:37 -0500

    Workshop2

---
+++ src/main/java/labcodeinspection/EmailApp.java
@@ -0,0 +1,32 @@
+package labcodeinspection;
+
+import java.util.Scanner;
+
+public class EmailApp {
+	
+	private EmailApp() {
+		throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter your first name: ");
+		String firstName = sc.nextLine();
+
+		System.out.print("Enter your last name: ");
+		String lastName = sc.nextLine();
+
+		System.out.print("\nDEPARTMENT CODE\n1. for sales\n2. for Development\n3. for accounting\nEnter code: ");
+
+		int depChoice = sc.nextInt();
+		sc.close();
+
+		Email email = new Email(firstName, lastName);
+		email.setDeparment(depChoice);
+		email.generateEmail();
+		email.showInfo();
+	}
+	
+
+}
